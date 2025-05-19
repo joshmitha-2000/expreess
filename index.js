a948c775a7a59a5536af2354c88f723725e4008d
Joshmitha-2000 <djoshmithadevarakonda@gmail.com>
2025-05-19 08:08:05 +0530

    vercel

--- index.js
+++ index.js
@@ -34,3 +34,9 @@ app.use("/wishlist", wishlist);
 app.use('/api/reviews', reviewRoutes);
 app.use("/orders", orderRoutes);
 module.exports = app;
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./index');
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
-
